83dc8df41c61e3c94059f0c2990d31200d99b9c2
vishal thakur <vishal2414@gmail.com>
2025-10-28 13:11:04 -0700

    sliding window hard

--- Problems/minimum_window_substring.py
+++ Problems/minimum_window_substring.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 '''
 Given two strings s and t of lengths m and n
 respectively, return the minimum window substring
@@ -33,7 +35,47 @@ one 'a', return empty string.
 
 
 def minimum_window_substring(s: str, t: str) -> str:
-    pass
+    left = 0
+    # Dictionary which holds the frequency
+    # of all the characters in t
+    t_counts = Counter(t)
+    # Number of unique characters in t that
+    # must be present in the window
+    required = len(t_counts)
+    # `formed` is used to keep track of how
+    # many unique characters in t are present
+    # in the current window in the required frequency.
+    formed = 0
+
+    # Dictionary which contains the frequency of
+    # characters in the current window.
+    window_counts = {}
+    # ans tuple of the form (window length, left, right)
+    ans = float("inf"), None, None
+    for right, char in enumerate(s):
+        window_counts[char] = window_counts.get(char, 0) + 1
+        # if the char is required, increase the formed count
+        if char in t and window_counts[char] == t_counts[char]:
+            formed += 1
+        # if all the required chars are in window
+        # start to shrink the window until its invalid again
+        while left <= right and formed == required:
+            # record the valid window length and left, right
+            # coordinates if minimum that the existing value
+            if right - left + 1 < ans[0]:
+                ans = (right - left + 1, left, right)
+            # window_counts is independently managed from
+            # t_counts, so all chars would end up here, just
+            # decrement count for all chars before deleting
+            left_char = s[left]
+            window_counts[left_char] -= 1
+            if left_char in t_counts and\
+                    window_counts[left_char] < t_counts[left_char]:
+                formed -= 1
+            # Move the left pointer ahead, this would help to look
+            # for a new window.
+            left += 1
+    return "" if ans[1] is None else s[ans[1]: ans[2] + 1]
 
 
 def tests():
@@ -53,7 +95,7 @@ def tests():
     t = "aa"
     res = minimum_window_substring(s, t)
     print(f"min window substring for strings '{s}'",
-          f"and '{t}' is {res}")
+          f"and '{t}' is {repr(res)}")
     assert res == ""
 
 
